0893f8d95092e849ae635b79b1a8cc9dbcfe40d4
Anjali-khushi <86797248+Anjali-khushi@users.noreply.github.com>
2021-07-21 10:36:22 +0530

    Update userdetails.php

--- userdetails.php
+++ userdetails.php
@@ -156,7 +156,7 @@ if(isset($_POST['submit']))
                 $rows=mysqli_fetch_assoc($result);
             ?>
             <form method="post" name="tcredit" class="tabletext" ><br>
-        <div>
+        <div class="table-responsive-sm">
             <table class="table table-striped table-condensed table-bordered">
                 <tr class="table-danger">
                     <th class="text-center">Id</th>
